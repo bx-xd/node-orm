2cb491577d7853d51b178815c9708f5b60ae5fe6
Antoine Brique <antoine.brique@gmail.com>
2024-12-11 17:06:16 +0100

    ✨ Feat(orm): adding hasMany relationship

--- example.js
+++ example.js
@@ -1,11 +1,18 @@
 import { Model, ORM } from './index.js';
 
+class Post extends Model {
+  constructor(data) {
+    super(data);
+  }
+}
 // Define User Model
 class User extends Model {
   constructor(data) {
     super(data);
+    this.hasMany(Post);
     this.addValidation('pseudo', this.validatePseudo);
   }
+
   validatePseudo(pseudo) {
     return pseudo.length > 3;
   }
@@ -36,6 +43,10 @@ async function main() {
     // Fetch a specific user by ID
     const foundUser = await User.findById(1);
     console.log('Found User:', foundUser);
+
+    // Get all posts of a user
+    const userPosts = await foundUser.posts;
+    console.log('User Posts:', userPosts);
 
     // Update an instance of User
     if (lastUser) {
